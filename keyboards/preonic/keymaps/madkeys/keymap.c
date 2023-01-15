942ccf07f17e9081fd9d58239e14be76ff71ffa5
Thomas Kaufmann <thomaskaufmann@bluewin.ch>
2023-01-15 13:46:34 +0100

    use madkeys base definitions

--- keyboards/preonic/keymaps/madkeys/keymap.c
+++ keyboards/preonic/keymaps/madkeys/keymap.c
@@ -15,6 +15,7 @@
  */
 
 #include QMK_KEYBOARD_H
+#include "madkeys.h"
 #include "muse.h"
 
 enum preonic_layers {
@@ -46,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl | GUI  | Alt  | Shift|Lower |    Space    |Raise |   /  | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_QUOT,
+[_QWERTY] = LAYOUT_preonic_grid_wrapper(
+  KC_GRV,  MADROW12345,    MADROW67890,    KC_BSPC,
+  KC_TAB,  MADROWQWERT,    MADROWYUIOP,    KC_DEL,
+  KC_ESC,  MADROWASDFG,    MADROWHJKL,     KC_SCLN, KC_ENT,
+  KC_LSFT, MADROWZXCVB,    MADROWNM,       MADROWCOMDOT,  KC_UP,   KC_QUOT,
   KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
