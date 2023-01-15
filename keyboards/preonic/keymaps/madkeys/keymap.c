493247afbc47f3658f1fc9b91dbd64936a355426
Thomas Kaufmann <thomaskaufmann@bluewin.ch>
2023-01-15 14:23:51 +0100

    use madkey rows on all layers

--- keyboards/preonic/keymaps/madkeys/keymap.c
+++ keyboards/preonic/keymaps/madkeys/keymap.c
@@ -48,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid_wrapper(
-  KC_GRV,  MADROW12345,    MADROW67890,    KC_BSPC,
-  KC_TAB,  MADROWQWERT,    MADROWYUIOP,    KC_DEL,
-  KC_ESC,  MADROWASDFG,    MADROWHJKL,     KC_SCLN, KC_ENT,
-  KC_LSFT, MADROWZXCVB,    MADROWNM,       MADROWCOMDOT,  KC_UP,   KC_QUOT,
+  KC_GRV,  MADROWNUM,             KC_BSPC,
+  KC_TAB,  MADROWQ,               KC_DEL,
+  KC_ESC,  MADROWA,               KC_ENT,
+  KC_LSFT, MADROWZ,      KC_UP,   KC_QUOT,
   KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
@@ -69,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |Brite |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------]'
  */
-[_LOWER] = LAYOUT_preonic_grid(
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR, KC_DEL,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______,
+[_LOWER] = LAYOUT_preonic_grid_wrapper(
+  KC_TILD, MADROWNUMLOW,         KC_BSPC,
+  KC_TILD, MADROWQLOW,           KC_DEL,
+  KC_DEL,  MADROWALOW,           KC_PIPE,
+  _______, MADROWZLOW,   KC_END, _______,
   _______, _______, _______, BACKLIT, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -90,11 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+[_RAISE] = LAYOUT_preonic_grid_wrapper(
+  KC_GRV,  MADROWNUMRAI,        KC_BSPC,
+  KC_GRV,  MADROWQRAI,          KC_DEL,
+  KC_DEL,  MADROWARAI,          KC_BSLS,
+  _______, MADROWZRAI, KC_PGDN, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
