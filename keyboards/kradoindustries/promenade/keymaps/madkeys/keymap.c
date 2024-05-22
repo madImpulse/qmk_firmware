a458856cdc373338498c39a3f532faff40400055
Thomas Kaufmann <thomaskaufmann@bluewin.ch>
2024-05-22 20:29:22 +0200

    adjust layout comment

--- keyboards/kradoindustries/promenade/keymaps/madkeys/keymap.c
+++ keyboards/kradoindustries/promenade/keymaps/madkeys/keymap.c
@@ -37,40 +37,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
      * | LCTRL  | A      | S      | D      | F      | G      |        |        | H      | J      | K      | L      | ;      | ENTER  |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
-     * | LSHIFT | Z      | X      | C      | V      | B      | END    | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
+     * | LSHIFT | Z      | X      | C      | V      | B      | PG UP  | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
      * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
      * | LCTRL  | LGUI   | FN     | LALT   | SPACE  | SPACE  | SPACE  | SPACE  | SPACE  | LEFT   | DOWN   | UP     | RIGHT  | RCTRL  |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
     [_BASE] = LAYOUT_promenade_grid_wrapper(
         KC_ESC,  MADROW12345,                                      KC_MINS, KC_EQL,  MADROW67890,                                      KC_BSPC,
-        KC_TAB,  MADROWQWERT,                                      KC_LBRC, KC_RBRC, MADROWYUIOP,                                      KC_QUOT,
+        KC_TAB,  MADROWQWERT,                                      KC_LBRC, KC_RBRC, MADROWYUIOP,                                      KC_DEL,
         MADCTL,  MADROWASDFG,                                                        MADROWHJKL, MADROWSEMICOL,                        LCTL_T(KC_QUOT),
-        KC_LSFT, MADROWZXCVB,                                      KC_END,  KC_PGDN, MADROWNMCDS,                                      KC_RSFT,
-        KC_LCTL, KC_LGUI, MADLALTENT, MO(_ADJUST), KC_SPC,  LT(_LOWER, KC_SPC), KC_SPC,  LT(_RAISE, KC_SPC),  MADRALTENT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCTL
+        KC_LSFT, MADROWZXCVB,                                      KC_PGUP,  KC_PGDN, MADROWNMCDS,                                      KC_RSFT,
+        KC_LCTL, MO(_ADJUST), KC_LGUI, MADLALTENT, LT(_LOWER, KC_SPC), KC_SPC,  KC_SPC,  LT(_RAISE, KC_SPC),  MADRALTENT, KC_LEFT, KC_RGHT, KC_UP, KC_DOWN
     ),
 
     [_LOWER] = LAYOUT_promenade_grid_wrapper(
-        _______,  MADROW12345LOW,                                  KC_MINS, KC_EQL,  MADROW67890LOW,                                   _______,
-        _______,  MADROWQWERTLOW,                                  KC_LBRC, KC_RBRC, MADROWYUIOPLOW,                                   _______,
+        _______,  MADROW12345LOW,                                  _______, _______,  MADROW67890LOW,                                   _______,
+        _______,  MADROWQWERTLOW,                                  _______, _______, MADROWYUIOPLOW,                                   _______,
         _______,  MADROWASDFGLOW,                                                     MADROWHJKLLOW, MADROWSEMICOLLOW,                 _______,
-        _______,  MADROWZXCVBLOW,                                   KC_END,  KC_PGDN, MADROWNMCDSLOW,                                  _______,
-        _______,  _______, _______, MO(_ADJUST), _______,  _______,      _______,      MO(_ADJUST),  _______, _______, _______,   _______, _______
+        _______,  MADROWZXCVBLOW,                                   _______,  _______, MADROWNMCDSLOW,                                  _______,
+        _______,  _______, _______, _______, _______,  _______,      _______,      MO(_ADJUST),  _______, _______, _______,   _______, _______
     ),
 
     [_RAISE] = LAYOUT_promenade_grid_wrapper(
-        _______,  MADROW12345RAI,                                  KC_MINS, KC_EQL,  MADROW67890RAI,                                   _______,
-        MADTABRAI,  MADROWQWERTRAI,                                  KC_LBRC, KC_RBRC, MADROWYUIOPRAI,                                   _______,
+        _______,  MADROW12345RAI,                                  _______, _______,  MADROW67890RAI,                                   _______,
+        MADTABRAI,  MADROWQWERTRAI,                                  KC_LPRN, KC_RPRN, MADROWYUIOPRAI,                                   _______,
         MADCAPSRAI,  MADROWASDFGRAI,                                                    MADROWHJKLRAI, MADROWSEMICOLRAI,                  KC_ENT,
-        MADLSFTRAI,  MADROWZXCVBRAI,                                  KC_END,  KC_PGDN, MADROWNMCDSRAI,                                   _______,
-        _______,  _______, _______, MO(_ADJUST), _______,  MO(_ADJUST),      _______,      _______,  _______, _______, _______,   _______, _______
+        MADLSFTRAI,  MADROWZXCVBRAI,                                  KC_HOME,  KC_END, MADROWNMCDSRAI,                                   _______,
+        _______,  _______, _______, _______, _______,  MO(_ADJUST),      _______,      _______,  _______, _______, _______,   _______, _______
     ),
 
     [_ADJUST] = LAYOUT_all(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______,
+        _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, KC_MPLY, KC_MSTP, _______, _______, _______, _______, KC_PSCR, _______,
         QK_BOOT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,                   _______, _______, _______, _______, _______, QK_BOOT,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, _______,
+        _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, _______,
         _______, _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______, QK_BOOT
     )
 };
